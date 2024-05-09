1a390bc2b9ee6c48c5da21fc0218c6706bc3fbbf
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-05-09 19:02:23 +0500

    template

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('It works');
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
